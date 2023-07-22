fc575393a822b78b53381fc226cd19fd2c08e530
clover <threeleafclover9@gmail.com>
2023-07-22 22:16:40 +0900

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "Hi about page";
+});
+
+Route::get('/contact', function () {
+    return "hi I am contact";
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "This is post number " . $id . " " . $name;
+});
+
+Route::get('/admin/posts/example', array('as' => 'admin.home', function () {
+    $url = \route('admin.home');
+    return "this url is " . $url;
+}));
