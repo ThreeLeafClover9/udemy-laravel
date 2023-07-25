f910e241d3bacf6ecd925418e3ee3ffbe44b9cdb
clover <ThreeLeafClover9@gmail.com>
2023-07-25 15:01:27 +0900

    eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -46,17 +47,94 @@ Route::get('/insert', function () {
     DB::insert('insert into posts (title, content) values (?, ?)', ['PHP with Laravel', 'Laravel is the best thing that has happened to PHP']);
 });
 
-Route::get('/read', function () {
+Route::get('/select', function () {
     $posts = DB::select('select * from posts where id = ?', [1]);
     return $posts;
 });
 
-Route::get('/update', function () {
-    $affected = DB::update('update posts set title = "Update title" where id = ?', [1]);
-    return $affected;
+Route::get('/all', function () {
+    $posts = Post::all();
+    return $posts;
 });
 
+Route::get('/find', function () {
+    $post = Post::find(1);
+    return $post;
+});
+
+Route::get('/where', function () {
+    $posts = Post::where('id', 1)->orderByDesc('id')->take(1)->get();
+    return $posts;
+});
+
+Route::get('/find-or-fail', function () {
+    $post = Post::findOrFail(0);
+    return $post;
+});
+
+Route::get('/first-or-fail', function () {
+    $post = Post::where('is_admin', '<', 50)->firstOrFail();
+    return $post;
+});
+
+//Route::get('/create', function () {
+//    $post = new Post();
+//    $post->title = 'New Eloquent title insert';
+//    $post->content = 'Wow eloquent is really cool, look at this content';
+//    $post->save();
+//});
+Route::get('/create', function () {
+    Post::create([
+        'title' => 'the create method',
+        'content' => 'WOW I\'m learning a lot with Edwin Diaz',
+    ]);
+});
+
+//Route::get('/update', function () {
+//    $affected = DB::update('update posts set title = "Update title" where id = ?', [1]);
+//    return $affected;
+//});
+//Route::get('/update', function () {
+//    $post = Post::find(1);
+//    $post->title = 'New Eloquent title insert 2';
+//    $post->content = 'Wow eloquent is really cool, look at this content 2';
+//    $post->save();
+//});
+Route::get('/update', function () {
+    Post::where('id', 1)
+        ->where('is_admin', 0)
+        ->update([
+            'title' => 'NEW PHP TITLE',
+            'content' => 'I love my instructor Edwin'
+        ]);
+});
+
+//Route::get('/delete', function () {
+//    $deleted = DB::delete('delete from posts where id = ?', [1]);
+//    return $deleted;
+//});
+//Route::get('/delete', function () {
+//    $post = Post::find(1);
+//    $post->delete();
+//});
 Route::get('/delete', function () {
-    $deleted = DB::delete('delete from posts where id = ?', [1]);
-    return $deleted;
+    Post::destroy(1);
+    Post::destroy([4, 5]);
+    Post::where('is_admin', 0)->delete();
+});
+
+Route::get('/with-trashed', function () {
+    return Post::withTrashed()->where('is_admin', 0)->get();
+});
+
+Route::get('/only-trashed', function () {
+    return Post::onlyTrashed()->where('is_admin', 0)->get();
+});
+
+Route::get('/restore', function () {
+    Post::withTrashed()->where('is_admin', 0)->restore();
+});
+
+Route::get('/force-delete', function () {
+    Post::onlyTrashed()->where('is_admin', 0)->forceDelete();
 });
